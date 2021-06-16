90c87079b4827a8eabd2bcb972e4d00ba332db77
ritik.tanwar00@gmail.com <ritik.tanwar00@gmail.com>
2021-06-17 00:50:13 +0530

    node version added

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,4 @@
-const baseURL =
-  location && location.hostname === "localhost"
-    ? "http://localhost:3000"
-    : "https://flipkart-admins-apps.herokuapp.com";
+const baseURL = "https://flipkart-admins-apps.herokuapp.com";
 
 export const api = `${baseURL}/api`;
 export const generatePublicURL = (filename) => {
